e6f7d06bb74c8afa769a6a79ede306c9839279a9
jeangalvis <campusjean2023@gmail.com>
2023-12-04 14:53:24 -0500

    Fibonacci

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿Console.Write("Ingrese n: ");
+int valor = Convert.ToInt32(Console.ReadLine());
+int temporal1 = 0;
+int temporal2 = 1;
+for(int i = 1; i<valor; i++){
+    int suma = temporal1 + temporal2;
+    temporal1 = temporal2;
+    temporal2 = suma;
+}
+Console.WriteLine(temporal2);
